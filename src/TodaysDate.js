2160985190c350ba19f6ba87f9a554d449fd52af
Carlos H. Lopez Carrillo <carlos.lopez@tress.com.mx>
2020-04-15 13:54:44 -0700

    third

--- src/TodaysDate.js
+++ src/TodaysDate.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Input } from "antd";
+import "antd/dist/antd.css";
 
 const TodaysDate = () => (
   <div>
     {`Today's date is ${Date()}`}
-    <Input />
+    <Input.Password />
   </div>
 );
 export default TodaysDate;
